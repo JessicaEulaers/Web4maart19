5facb0b17731995be8bf5d66eb7a08fd9e78a44d
JessicaEulaers <33091372+JessicaEulaers@users.noreply.github.com>
2019-03-25 16:04:30 +0100

    ex 1 part 1

--- Spring-BlockChain/Spring-BlockChain/src/main/java/edu/ap/spring/service/BlockChain.java
+++ Spring-BlockChain/Spring-BlockChain/src/main/java/edu/ap/spring/service/BlockChain.java
@@ -12,42 +12,45 @@ import net.minidev.json.JSONObject;
 @Service
 @Scope("singleton")
 public class BlockChain {
-	
+
 	private ArrayList<Block> blocks = new ArrayList<Block>();
+
 	private int difficulty = 1;
-	
-	public BlockChain() {}
-	
-	public void setSecurity() {
-		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); //setup Bouncey castle as a Security Provider	
+
+	public BlockChain() {
 	}
-	
+
+	public void setSecurity() {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); // setup Bouncey castle as a
+																						// Security Provider
+	}
+
 	public ArrayList<Block> getBlocks() {
 		return this.blocks;
 	}
 
 	public Boolean isChainValid() {
-		Block currentBlock; 
+		Block currentBlock;
 		Block previousBlock;
 		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
-		
-		//loop through blockchain to check hashes:
-		for(int i = 1; i < blocks.size(); i++) {
-			
+
+		// loop through blockchain to check hashes:
+		for (int i = 1; i < blocks.size(); i++) {
+
 			currentBlock = blocks.get(i);
-			previousBlock = blocks.get(i-1);
-			//compare registered hash and calculated hash:
-			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+			previousBlock = blocks.get(i - 1);
+			// compare registered hash and calculated hash:
+			if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
 				System.out.println("# Current Hashes not equal");
 				return false;
 			}
-			//compare previous hash and registered previous hash
-			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+			// compare previous hash and registered previous hash
+			if (!previousBlock.hash.equals(currentBlock.previousHash)) {
 				System.out.println("# Previous Hashes not equal");
 				return false;
 			}
-			//check if hash is solved
-			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+			// check if hash is solved
+			if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
 				System.out.println("# This block hasn't been mined");
 				return false;
 			}
@@ -55,17 +58,17 @@ public class BlockChain {
 		System.out.println("Blockchain is valid");
 		return true;
 	}
-	
+
 	public void addBlock(Block newBlock) {
 		newBlock.mineBlock(difficulty);
 		this.blocks.add(newBlock);
 	}
 
 	public String toJSON() {
-		
+
 		JSONObject jsonObj = new JSONObject();
-		for(int i = 0; i < blocks.size(); i++) {
-			
+		for (int i = 0; i < blocks.size(); i++) {
+
 			Block currentBlock = blocks.get(i);
 			JSONObject blockObj = new JSONObject();
 			blockObj.put("hash", currentBlock.hash);
@@ -75,7 +78,7 @@ public class BlockChain {
 			blockObj.put("timeStamp", currentBlock.timeStamp);
 			JSONObject[] trs = new JSONObject[currentBlock.getTransactions().size()];
 			int j = 0;
-			for(Transaction t : currentBlock.getTransactions()) {
+			for (Transaction t : currentBlock.getTransactions()) {
 				JSONObject tr = new JSONObject();
 				tr.put("recipient", t.recipient.toString());
 				tr.put("sender", t.sender.toString());
@@ -85,9 +88,9 @@ public class BlockChain {
 				trs[j] = tr;
 				j++;
 			}
-			
+
 			blockObj.put("transactions", trs);
-			jsonObj.put("block" + i, blockObj.toString()); 
+			jsonObj.put("block" + i, blockObj.toString());
 		}
 
 		return jsonObj.toJSONString();
--- Spring-BlockChain/Spring-BlockChain/src/main/java/edu/ap/spring/transaction/Transaction.java
+++ Spring-BlockChain/Spring-BlockChain/src/main/java/edu/ap/spring/transaction/Transaction.java
@@ -6,24 +6,21 @@ import edu.ap.spring.service.BlockChain;
 import edu.ap.spring.service.StringUtil;
 
 public class Transaction {
-	
+
 	public String transactionId; // contains a hash of transaction
 	public PublicKey sender; // senders address/public key.
 	public PublicKey recipient; // recipients address/public key.
 	public float value; // contains the amount we wish to send to the recipient.
 	public byte[] signature; // this is to prevent anybody else from spending funds in our wallet.
-		
-	public Transaction() {}
 
-	public Transaction(PublicKey sender, PublicKey recipient , float value) {
+	public Transaction() {
+	}
+
+	public Transaction(PublicKey sender, PublicKey recipient, float value) {
 		this.sender = sender;
 		this.recipient = recipient;
 		this.value = value;
-		this.transactionId = StringUtil.applySha256( 
-				sender.toString() +
-				recipient.toString() +
-				value
-				);
+		this.transactionId = StringUtil.applySha256(sender.toString() + recipient.toString() + value);
 	}
 
 	public void setSender(PublicKey sender) {
@@ -33,7 +30,7 @@ public class Transaction {
 	public PublicKey getSender() {
 		return this.sender;
 	}
-	
+
 	public void setRecipient(PublicKey recipient) {
 		this.recipient = recipient;
 	}
@@ -41,7 +38,7 @@ public class Transaction {
 	public PublicKey getRecipient() {
 		return this.recipient;
 	}
-	
+
 	public void setValue(float value) {
 		this.value = value;
 	}
@@ -49,22 +46,24 @@ public class Transaction {
 	public float getValue() {
 		return this.value;
 	}
-	
+
 	public boolean processTransaction(BlockChain bChain) {
-		if(verifySignature() == false) {
+		if (verifySignature() == false) {
 			System.out.println("# Transaction Signature failed to verify");
 			return false;
 		}
 		return true;
 	}
-	
+
 	public void generateSignature(PrivateKey privateKey) {
-		String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value);
-		signature = StringUtil.applyECDSASig(privateKey, data);		
+		String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient)
+				+ Float.toString(value);
+		signature = StringUtil.applyECDSASig(privateKey, data);
 	}
-	
+
 	public boolean verifySignature() {
-		String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value);
+		String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient)
+				+ Float.toString(value);
 		return StringUtil.verifyECDSASig(sender, data, signature);
 	}
 }
